751323847aac7563ffc42ce9c63dbd6da9c1a979
Martyn <martynrdavies@gmail.com>
2013-09-04 18:32:27 +0100

    update example

--- example/breakdown.js
+++ example/breakdown.js
@@ -2,10 +2,10 @@ var sendstrip = require('../sendstrip');
 var email = "\"SendGrid Test Yeah\" <send.grid.test@gmail.com>";
 
 sendstrip(email, function(err, name, address, to ,domain){
-	console.log(err);
-	console.log(name);
-	console.log(address);
-	console.log(to);
-	console.log(domain);
+  console.log(err);
+  console.log(name);
+  console.log(address);
+  console.log(to);
+  console.log(domain);
 });
 
