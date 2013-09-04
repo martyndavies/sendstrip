cb203aa41d593a2960b4279a707cab2e19ab9ea7
Martyn <martynrdavies@gmail.com>
2013-09-04 18:30:47 +0100

    strange indentation

--- sendstrip.js
+++ sendstrip.js
@@ -2,18 +2,18 @@ var _ = require('underscore');
 var completeString, err, address, amountOfSplits, emailAddress, senderName, splitEmail, to, domain;
 
 module.exports = function(rawEmail, cb) {
-	"use strict";
+  "use strict";
 
-	if (rawEmail) {
-		completeString	= rawEmail.replace(/</g, '').replace(/>/g, '').split(' ');
-		senderName 			= _.initial(completeString, 1).toString().replace(/,/g, ' ').replace(/"/g, '');
-		emailAddress 		= _.last(completeString, 1).toString();
-		splitEmail 			= emailAddress.split('@');
-		to 							= splitEmail[0];
-		domain 					= splitEmail[1];
-		
-		cb(err=null, senderName, emailAddress, to, domain);
-	} else {
-		cb(new Error('The module errored'), null, null, null, null);
-	}
+  if (rawEmail) {
+    completeString  = rawEmail.replace(/</g, '').replace(/>/g, '').split(' ');
+    senderName      = _.initial(completeString, 1).toString().replace(/,/g, ' ').replace(/"/g, '');
+    emailAddress    = _.last(completeString, 1).toString();
+    splitEmail      = emailAddress.split('@');
+    to              = splitEmail[0];
+    domain          = splitEmail[1];
+    
+    cb(err=null, senderName, emailAddress, to, domain);
+  } else {
+    cb(new Error('The module errored'), null, null, null, null);
+  }
 };
